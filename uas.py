08033ae9bd0c6706b2c0627bbf5b11731acc878f
asrizalnova <60204762+asrizalnova@users.noreply.github.com>
2023-12-10 12:23:56 +0700

    Update uas.py

--- uas.py
+++ uas.py
@@ -11,11 +11,12 @@ from sklearn.ensemble import RandomForestRegressor
 dataframe = pd.read_csv("lettuce_dataset.csv", encoding='latin-1')
 dataframe['Date'] = pd.to_datetime(dataframe['Date'])
 
-# Sidebar
+
+# Sidebar menu
 st.sidebar.title("Lettuce Growth Prediction")
 
 # Sidebar menu
-menu = st.sidebar.selectbox('Menu', ['Home', 'Dataset Overview', 'Correlation Heatmap', 'Histograms', 'Scatter Plots', 'Box Plots', 'Prediction'])
+menu = st.sidebar.selectbox('Menu', ['Home', 'Exploratory Data Analysis', 'Prediction'])
 
 # Content
 st.title("Lettuce Growth")
@@ -23,140 +24,169 @@ st.title("Lettuce Growth")
 if menu == 'Home':
     st.image('selada.jpg', caption='Lettuce Growth', use_column_width=True)
 
-elif menu == 'Dataset Overview':
-    st.subheader("Dataset Overview")
-    st.write("Top 10 Rows of the Dataset:")
-    st.write(dataframe.head(10))
+elif menu == 'Exploratory Data Analysis':
+    # Submenu selection
+    eda_submenu = st.selectbox('Pilih Visualisasi yang ingin di lihat:', ['Dataset Overview', 'Correlation Heatmap', 'Histograms', 'Scatter Plots', 'Box Plots'])
 
-    st.write("Bottom 10 Rows of the Dataset:")
-    st.write(dataframe.tail(10))
+    if eda_submenu == 'Dataset Overview':
+        st.subheader("Dataset Overview")
+        st.write("Top 10 Rows of the Dataset:")
+        st.write(dataframe.head(10))
 
-    st.write("Dataset Shape:")
-    st.write(dataframe.shape)
+        st.write("Bottom 10 Rows of the Dataset:")
+        st.write(dataframe.tail(10))
 
-    st.write("Dataset Info:")
-    st.write(dataframe.info())
+        st.write("Dataset Shape:")
+        st.write(dataframe.shape)
 
-    st.write("Summary Statistics:")
-    st.write(dataframe.describe())
+        st.write("Dataset Info:")
+        st.write(dataframe.info())
 
-    st.write("Missing Values Count:")
-    st.write(dataframe.isna().sum())
+        st.write("Summary Statistics:")
+        st.write(dataframe.describe())
 
-    st.write("Duplicate Rows Count:")
-    st.write(dataframe.duplicated().sum())
+        st.write("Missing Values Count:")
+        st.write(dataframe.isna().sum())
 
-elif menu == 'Correlation Heatmap':
-    st.subheader("Correlation Heatmap")
-    plt.figure(figsize=(8, 8))
-    sns.heatmap(dataframe.corr(), annot=True, cmap="Reds", fmt=".2f")
-    st.pyplot()
+        st.write("Duplicate Rows Count:")
+        st.write(dataframe.duplicated().sum())
 
-elif menu == 'Histograms':
-    st.subheader("Histograms")
-    st.write("Temperature Histogram")
-    sns.histplot(data=dataframe, x="Temperature (°C)", kde=True, color="blue")
-    st.pyplot()
+    elif eda_submenu == 'Correlation Heatmap':
+        st.subheader("Correlation Heatmap")
+        plt.figure(figsize=(8, 8))
+        sns.heatmap(dataframe.corr(), annot=True, cmap="Reds", fmt=".2f")
+        st.pyplot()
 
-    st.write("Humidity Histogram")
-    sns.histplot(data=dataframe, x="Humidity (%)", kde=True, color="red")
-    st.pyplot()
+    elif eda_submenu == 'Histograms':
+        st.subheader("Histograms")
+        st.write("Temperature Histogram")
+        sns.histplot(data=dataframe, x="Temperature (°C)", kde=True, color="blue")
+        st.pyplot()
 
-    st.write("TDS Value Histogram")
-    sns.histplot(data=dataframe, x="TDS Value (ppm)", kde=True, color="green")
-    st.pyplot()
+        st.write("Humidity Histogram")
+        sns.histplot(data=dataframe, x="Humidity (%)", kde=True, color="red")
+        st.pyplot()
 
-    st.write("Growth Days Histogram")
-    sns.histplot(data=dataframe, x="Growth Days", kde=True, color="brown")
-    st.pyplot()
+        st.write("TDS Value Histogram")
+        sns.histplot(data=dataframe, x="TDS Value (ppm)", kde=True, color="green")
+        st.pyplot()
 
-    st.write("pH Level Histogram")
-    sns.histplot(data=dataframe, x="pH Level", kde=True, color="purple")
-    st.pyplot()
+        st.write("Growth Days Histogram")
+        sns.histplot(data=dataframe, x="Growth Days", kde=True, color="brown")
+        st.pyplot()
 
-    st.write("Monthly Growth Days Count")
-    sns.countplot(data=dataframe, x=dataframe["Date"].dt.month.map({8: "August", 9: "September"}), color="brown")
-    st.pyplot()
+        st.write("pH Level Histogram")
+        sns.histplot(data=dataframe, x="pH Level", kde=True, color="purple")
+        st.pyplot()
 
-elif menu == 'Scatter Plots':
-    st.subheader("Scatter Plots")
-    st.write("Growth Days Vs Temperature")
-    fig = px.scatter(dataframe, x="Temperature (°C)", y="Growth Days", marginal_x="histogram", marginal_y="histogram", title="Growth Days Vs Temperature", color_discrete_sequence=["red"])
-    st.plotly_chart(fig)
+        st.write("Monthly Growth Days Count")
+        sns.countplot(data=dataframe, x=dataframe["Date"].dt.month.map({8: "August", 9: "September"}), color="brown")
+        st.pyplot()
 
-    st.write("Growth Days Vs TDS Value")
-    fig = px.scatter(dataframe, x="TDS Value (ppm)", y="Growth Days", marginal_x="histogram", marginal_y="histogram", title="Growth Days Vs TDS Value (ppm)", color_discrete_sequence=["green"])
-    st.plotly_chart(fig)
+    elif eda_submenu == 'Scatter Plots':
+        st.subheader("Scatter Plots")
+        st.write("Growth Days Vs Temperature")
+        fig = px.scatter(dataframe, x="Temperature (°C)", y="Growth Days", marginal_x="histogram", marginal_y="histogram", title="Growth Days Vs Temperature", color_discrete_sequence=["red"])
+        st.plotly_chart(fig)
 
-    st.write("Growth Days Vs Date")
-    fig = px.scatter(dataframe, x="Date", y="Growth Days", marginal_x="histogram", marginal_y="histogram", title="Growth Days Vs Date", color_discrete_sequence=["purple"])
-    st.plotly_chart(fig)
+        st.write("Growth Days Vs TDS Value")
+        fig = px.scatter(dataframe, x="TDS Value (ppm)", y="Growth Days", marginal_x="histogram", marginal_y="histogram", title="Growth Days Vs TDS Value (ppm)", color_discrete_sequence=["green"])
+        st.plotly_chart(fig)
 
-elif menu == 'Box Plots':
-    st.subheader("Box Plots")
-    st.write("Growth Days Vs pH Level")
-    fig = px.box(dataframe, x="pH Level", y="Growth Days", title="Growth Days Vs pH Level", color_discrete_sequence=["blue"])
-    st.plotly_chart(fig)
+        st.write("Growth Days Vs Date")
+        fig = px.scatter(dataframe, x="Date", y="Growth Days", marginal_x="histogram", marginal_y="histogram", title="Growth Days Vs Date", color_discrete_sequence=["purple"])
+        st.plotly_chart(fig)
 
-    st.write("Growth Days Vs Humidity")
-    fig = px.box(dataframe, x="Humidity (%)", y="Growth Days", title="Growth Days Vs Humidity (%)", color_discrete_sequence=["black"])
-    st.plotly_chart(fig)
+    elif eda_submenu == 'Box Plots':
+        st.subheader("Box Plots")
+        st.write("Growth Days Vs pH Level")
+        fig = px.box(dataframe, x="pH Level", y="Growth Days", title="Growth Days Vs pH Level", color_discrete_sequence=["blue"])
+        st.plotly_chart(fig)
 
+        st.write("Growth Days Vs Humidity")
+        fig = px.box(dataframe, x="Humidity (%)", y="Growth Days", title="Growth Days Vs Humidity (%)", color_discrete_sequence=["green"])
+        st.plotly_chart(fig)
 
 elif menu == 'Prediction':
     st.subheader("Lettuce Growth Prediction")
 
     # Date Range Input
-    date_range = st.date_input("Pilih range tanggal (Data hanya tersedia dari 3 Agustus - 19 September)", [dataframe['Date'].min(), dataframe['Date'].max()])
+    date_range = st.date_input("Select Date Range (Data is available from August 3 - September 19)", [dataframe['Date'].min(), dataframe['Date'].max()])
 
-# Check if the date_range has two elements
-if len(date_range) == 2:
-    start_date = np.datetime64(date_range[0])
-    end_date = np.datetime64(date_range[1])
+    # Select Features
+    features = st.multiselect("Select Features:", ['Temperature (°C)', 'Humidity (%)', 'TDS Value (ppm)', 'pH Level'], default=['Temperature (°C)'])
 
-    # Filter dataframe range tanggal
-    filtered_dataframe = dataframe[(dataframe['Date'] >= start_date) & (dataframe['Date'] <= end_date)]
+    # Create sliders for each selected feature
+    sliders = {}
+    for feature in features:
+        value = st.slider(f"Estimator {feature} range", min_value=1, max_value=50, value=[1,3], step=1)
+        sliders[feature] = (value)
 
-    st.write("Dataframe yang Difilter:")
-    st.write(filtered_dataframe)
-    # Parameter input untuk model Random Forest
-    n_estimators = st.slider("Jumlah Estimator (n_estimators)", min_value=1, max_value=500, value=100, step=1)
+    if len(date_range) == 2:
+        start_date = np.datetime64(date_range[0])
+        end_date = np.datetime64(date_range[1])
 
-    # memilih target features dan target
-    features = ['Temperature (°C)', 'Humidity (%)', 'TDS Value (ppm)', 'pH Level']
-    target = 'Growth Days'
+        # Filter dataframe by date range
+        filtered_dataframe = dataframe[(dataframe['Date'] >= start_date) & (dataframe['Date'] <= end_date)]
 
-    # memisah data ke variable features dan target
-    X = filtered_dataframe[features]
-    y = filtered_dataframe[target]
+        st.write("Filtered DataFrame:")
+        st.write(filtered_dataframe)
 
-    # memisah data ke training dan testing sets
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+        # Separate data into features and target
+        X = filtered_dataframe[features]
+        y = filtered_dataframe['Growth Days']
 
-    # inisialisasi dan train Random Forest
-    model = RandomForestRegressor(n_estimators=n_estimators, random_state=42)
-    model.fit(X_train, y_train)
+        # Check for missing values
+        st.write("Missing Values in X:")
+        st.write(X.isnull().sum())
 
-    predictions = model.predict(X_test)
+        st.write("Missing Values in y:")
+        st.write(y.isnull().sum())
 
-    # menampilkan predictions dan actual values
-    st.write("Predictions vs Data Real:")
-    result_df = pd.DataFrame({'Actual': y_test, 'Predicted': predictions})
-    st.write(result_df)
+        # Check data types
+        st.write("Data Types in X:")
+        st.write(X.dtypes)
 
-    # menampilkan scatter plot of actual vs predicted values
-    st.subheader("Scatter Plot of Actual vs Predicted Values")
-    fig = px.scatter(result_df, x='Actual', y='Predicted', labels={'Actual': 'Actual Values', 'Predicted': 'Predicted Values'})
-    st.plotly_chart(fig)
+        # Check target variable
+        st.write("Data Types in y:")
+        st.write(y.dtypes)
 
-    # Menampilkan feature importance
-    st.subheader("Feature Importance")
-    feature_importance = model.feature_importances_
-    feature_importance_df = pd.DataFrame({'Feature': features, 'Importance': feature_importance})
-    fig = px.bar(feature_importance_df, x='Feature', y='Importance', title='Feature Importance')
-    st.plotly_chart(fig)
-else:
-    st.warning("Silahkan pilih 2 tanggal/ jika ingin 1 tanggal klik tanggal tsb 2x.")
+        # Separate data into training and testing sets
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+        # Initialize and train Random Forest model
+        total_estimators = sum(max_value for _, max_value in sliders.values())
+        model = RandomForestRegressor(n_estimators=total_estimators, random_state=42)
+        model.fit(X_train, y_train)
+
+
+        # Make predictions
+        predictions = model.predict(X_test)
+
+        # Display predictions and actual values
+        st.write("Predictions vs Actual Values:")
+        result_df = pd.DataFrame({'Actual': y_test, 'Predicted': predictions})
+        st.write(result_df)
+
+        # Display scatter plot of actual vs predicted values
+        st.subheader("Scatter Plot of Actual vs Predicted Values")
+        fig = px.scatter(result_df, x='Actual', y='Predicted', labels={'Actual': 'Actual Values', 'Predicted': 'Predicted Values'})
+        st.plotly_chart(fig)
+
+        # Display feature importance
+        st.subheader("Feature Importance")
+        feature_importance = model.feature_importances_
+        feature_importance_df = pd.DataFrame({'Feature': features, 'Importance': feature_importance})
+        st.write(feature_importance_df)
+
+        # Create bar chart for feature importance
+        st.subheader("Bar Chart of Feature Importance")
+        fig_bar = px.bar(feature_importance_df, x='Feature', y='Importance', title='Feature Importance')
+        st.plotly_chart(fig_bar)
+
+
+    else:
+        st.warning("Silahkan pilih 2 tanggal/ jika ingin 1 tanggal klik tanggal tsb 2x.")
 
 
 st.set_option('deprecation.showPyplotGlobalUse', False)
